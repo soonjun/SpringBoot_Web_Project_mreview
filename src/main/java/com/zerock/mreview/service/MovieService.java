c0cebdd6883dbb3a613968fec27e8cf3a747dadb
Sunjun Ok <remedime0@gmail.com>
2023-02-20 14:10:30 +0900

    조회 페이지와 영화 리뷰

--- src/main/java/com/zerock/mreview/service/MovieService.java
+++ src/main/java/com/zerock/mreview/service/MovieService.java
@@ -18,6 +18,8 @@ public interface MovieService {
 
     PageResultDTO<MovieDTO, Object[]> getList(PageRequestDTO requestDTO); //목록 처리
 
+    MovieDTO getMovie(Long mno);
+
     default MovieDTO entitiesToDTO(Movie movie, List<MovieImage> movieImages, Double avg, Long reviewCnt) {
 
         MovieDTO movieDTO = MovieDTO.builder()
